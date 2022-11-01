227359e02ab22c139b0110ee0f2596bd4a827ead
svolts <65930465+svolts@users.noreply.github.com>
2022-11-01 13:52:20 +0900

    22-11-01

---
+++ csharp/programmers/약수의개수와덧셈.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class Solution
+{
+    static int solution(int left,int right) {
+        int sum = 0;
+        for(int i = left; i <= right; i++)
+        {
+            int count = 0;
+            for(int j = 1; j <= i; j++)
+            {
+                if (i % j == 0) count += 1;
+            }
+            if (count % 2 == 0) sum += i;
+            else sum -= i;
+        }
+        return sum;
+        
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine(solution(13, 17));
+    }
+}
