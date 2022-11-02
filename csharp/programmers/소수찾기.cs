8af268719210894a343a654de8d7b2c7967355f8
platypus46 <svolts2185@gmail.com>
2022-11-02 20:06:37 +0900

    22-11-02

--- csharp/programmers/소수찾기.cs
+++ csharp/programmers/소수찾기.cs
@@ -1,14 +1,27 @@
-﻿public class Solution
+﻿using System;
+using System.Linq;
+
+public class Solution
 {
-    public string solution(string s)
+    public int solution(int n)
     {
-        if (s.Length % 2 == 0)
+        int answer = 0;
+        bool[] arr = Enumerable.Repeat(true, n + 1).ToArray();
+        arr[0] = false;
+        arr[1] = false;
+
+
+        for (int i = 2; i <= Math.Sqrt(n); i++)
         {
-            return s[s.Length / 2 - 1].ToString() + s[s.Length / 2].ToString();
+            if (arr[i] == true)
+            {
+                for (int j = i + i; j < n + 1; j = j + i)
+                {
+                    arr[j] = false;
+                }
+            }
         }
-        else
-        {
-            return s[s.Length / 2].ToString();
-        }
+        answer = arr.Count(s => s == true);
+        return answer;
     }
 }
