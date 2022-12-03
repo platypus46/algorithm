a4737763c9996e186818cd3e4f832953a57c8c12
svolts <65930465+svolts@users.noreply.github.com>
2022-12-03 20:53:25 +0900

    22-12-03

--- python/programmers/명예의전당(1).py
+++ python/programmers/명예의전당(1).py
@@ -1,23 +1,13 @@
-import math
+def solution(k, score):
+    answer = []
 
-def get_num(n):
-    cnt=0
-
-    for i in range(1,int(math.sqrt(n))+1):
-        if n%i==0:
-            cnt+=1
-            if (i*i)<n:
-                cnt+=1
-
-    return cnt
-
-def solution(number,limit,power):
-    ans=0
-
-    for i in range(1,number+1):
-        if get_num(i)<=limit:
-            ans+=get_num(i)
+    for i in range(len(score)):
+        ls = score[:i + 1]
+        if len(ls) <= k:
+            ls.sort()
+            answer.append(ls[0])
         else:
-            ans+=power
+            ls.sort()
+            answer.append(ls[-k])
 
-    return ans
+    return answer
